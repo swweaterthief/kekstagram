a924446a7a4438ad5cab8874c91d14baad6ad9bb
Валентина Шевкун <vaniished@mail.ru>
2023-12-03 21:54:16 +0500

    Отрисуй меня полностью

---
+++ js/photos.js
@@ -0,0 +1,21 @@
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const installPhotoInGallery = (photo) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__img').alt = photo.description;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  return pictureElement;
+};
+
+const installPhotosInGallery = (photos) => {
+  const fragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    fragment.appendChild(installPhotoInGallery(photo));
+  });
+  pictures.appendChild(fragment);
+};
+
+export {installPhotosInGallery};
