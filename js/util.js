56675ca3a7bcfab84c33b0905bf3713842e1f8a4
Валентина Шевкун <vaniished@mail.ru>
2023-12-22 03:38:06 +0500

    Перламутровые пуговицы

--- js/util.js
+++ js/util.js
@@ -1,5 +1,7 @@
 const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+const DELAY = 500;
+
 export {randomInteger};
 
 const Keys = {
@@ -14,3 +16,28 @@ const isEscapeKey = (callback) => (evt) => {
 };
 
 export {isEscapeKey};
+
+const debounce = (cb) => {
+  let lastTimeout = null;
+
+  return (...args) => {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(() => {
+      cb(...args);
+    }, DELAY);
+  };
+};
+
+const shuffleArray = (array) => {
+  for (let indexOne = array.length - 1; indexOne > 0; indexOne--) {
+    const indexTwo = Math.floor(Math.random() * (indexOne + 1));
+    [array[indexOne], array[indexTwo]] = [array[indexTwo], array[indexOne]];
+  }
+  return array;
+};
+
+export {debounce};
+
+export {shuffleArray};
